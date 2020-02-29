d2a5403eee650488bd417720c810ff9c343d9981
SIGMA1425 <yahasho1425@gmail.com>
2020-02-29 17:31:18 +0900

    課題用のコード

--- sort/quick.c
+++ sort/quick.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 void swap(int *a, int*b)
 {
@@ -8,26 +10,131 @@ void swap(int *a, int*b)
     *b = tmp;
 }
 
+void deal(int *arr, int num);
+void deal2(int *arr, int start, int end, int* lr);
+void quick_sort(int *arr, int num, int left, int right);
+int* ary_cre(int x);
+void shell_sort(int* array, size_t size);
+
 int main(void)
 {
-    
-    
+    int i, n=10;
+    int *ary = ary_cre(n);
+    int lr[2];
+
+    for(i=0; i<n; i++)
+    {
+        printf("%d, ", ary[i]);
+    }
+    putchar('\n');
+
+    quick_sort(ary, n, 0, 4);
+
+    for(i=0; i<n; i++)
+    {
+        printf("%d, ", ary[i]);
+    }
+
+    free(ary);
+
+    return 0;
+
 }
 
 void deal(int *arr, int num)
 {
-    int l, r, p=arr[num/2];
+    int l = 0, r = num - 1, p=arr[num/2];
 
-    for(l=0;arr[l]<p;l++);
-    for(r=0;arr[r]>p;r--);
     while(l<=r)
     {
-        if(l<R)
+        for(;arr[l]<p;l++);
+        for(;arr[r]>p;r--);
+        if(l>r)
         {
             break;
         }
-        swap(&arr[l], arr[r]);
+        swap(&arr[l], &arr[r]);
         l++;
         r--;
     }   
+}
+
+int* ary_cre(int x)
+{
+    int i;
+    int *ary = malloc(x *sizeof(int));
+    srand(time(NULL));
+
+    for(i=0; i < x; i++)
+    {
+        ary[i] = rand()%10;
+    }
+
+    return ary;
+}
+
+void deal2(int *arr, int start, int end, int* lr)
+{
+    int l , r , p;
+    l = start;
+    r = end;
+    p = arr[(end+start)/2];
+
+    while(l<=r)
+    {
+        for(;arr[l]<p;l++);
+        for(;arr[r]>p;r--);
+        if(l>r)
+        {
+            break;
+        }
+        swap(&arr[l], &arr[r]);
+        l++;
+        r--;
+    }
+    lr[0] = l;
+    lr[1] = r; 
+    printf("p:%d\n", p);
+}
+
+void shell_sort(int* array, size_t size)
+{
+    // 最初の間隔 h を決める
+    // 1,4,13,40,121... のように、1 から始めて h=h*3+1 を満たす値を使う。
+    // これらの値の中で、size / 9 を超えない一番大きい値を最初の h とする。
+    size_t h = 1;
+    for( size_t h_tmp = 1; h_tmp < size / 9; h_tmp = h_tmp * 3 + 1 ){
+        h = h_tmp;
+    }
+
+    while( h > 0 ){
+        for( size_t i = h; i < size; ++i ){
+            int tmp = array[i];
+
+            if( array[i-h] > tmp ){  // 挿入が必要か先に調べる
+                size_t j = i;
+
+                do {
+                    array[j] = array[j-h];
+                    j -= h;
+                } while( j >= h && array[j-h] > tmp );
+
+                array[j] = tmp;
+            }
+        }
+        h /= 3;  // 間隔を縮める
+    }
+}
+
+void quick_sort(int *arr, int num, int left, int right)
+{
+    int l , r , p;
+    int lr[2];
+    if(l >= r)
+    {
+        return;
+    }
+    deal2(arr, left, right, lr);
+    quick_sort(arr, num, left, lr[1]);
+    quick_sort(arr, num, lr[0], right);
 }
--- sort/select_sort.c
+++ sort/select_sort.c
@@ -3,64 +3,114 @@
 #include <time.h>
 
 void selection_sort(int*, int, int);
+int exchange_sort(int*, int, int);
+int insertion_sort(int*, int, int);
+
 int is_sorted(int*, int, int);
 
-int main()
-{
-  int n = 10;
-  int *ary = malloc(n * sizeof(int));
+int main() {
+  int n = 33;
+  int *ary;
   int i;
-  
-  srand((unsigned int)time(NULL));
-  for(i=0; i<n; ++i) 
+  int asc = 0;
+
+  clock_t start, end;
+
+  FILE *fp = fopen("./clock_select.csv", "w+");
+  if(fp == NULL)
   {
-    ary[i] = rand() % 10;
-  }
-  for(i=0; i<n; ++i) 
+    printf("fileopen error\n");
+    exit(0);
+  } 
+  else
   {
-    printf("%d, ", ary[i]);
+    printf("fileopen success\n");
   }
-  printf("\n");
+  fprintf(fp, "index, time[ms], \n");
   
-  selection_sort(ary, n, 1);
-  
-  for(i=0; i<n; ++i) 
+  while (n <= 40000)
   {
-    printf("%d, ", ary[i]);
+    ary = malloc(n * sizeof(int));
+    srand((unsigned int)time(NULL));
+    for(i=0; i<n; ++i) {
+      ary[i] = rand() % 1000;
+    }
+
+    start = clock();
+    
+    selection_sort(ary, n, asc);
+    //exchange_sort(ary, n, asc);
+    //insertion_sort(ary, n, asc);
+
+    end = clock();
+
+    fprintf(fp, "%d, %lf, \n", n, (double)(end-start)/(double)(CLOCKS_PER_SEC));
+    free(ary);
+
+    n *= 2;
   }
-  printf("\n");
-  printf("%d\n", is_sorted(ary, 10, 1));
-  
-  free(ary);
+
+  fclose(fp);
   return 0;
 }
 
-void selection_sort(int *ary, int anum, int asc) 
-{
+void selection_sort(int *ary, int anum, int asc) {
   int i, j, tmp, mi;
   for(i=0; i<anum-1; ++i) {
     //printf("i:%d\n", i);
     mi = i;//暫定的な最大(小)値の位置
     for(j=i+1; j<anum; ++j) {
       //printf("j:%d\n", j);
-      if(ary[j] < ary[mi]) {
+      if(( asc && ary[j] < ary[mi]) ||
+         (!asc && ary[j] > ary[mi])) {
         mi = j;
       }
     }
-    printf("ary[%d] <--> ary[%d]\n", i, mi);
+    //printf("ary[%d] <--> ary[%d]\n", i, mi);
     tmp = ary[i];
     ary[i] = ary[mi];
     ary[mi] = tmp;
   }
 }
 
-int is_sorted(int *ary, int anum, int asc) 
-{
+int exchange_sort(int *ary, int anum, int asc) {
+  int i, j, tmp;
+  for(i=anum-1; i>=0; --i) {
+    for(j=0; j<i; ++j) {
+      if(( asc && ary[j] > ary[j+1]) ||
+         (!asc && ary[j] < ary[j+1])) {
+        tmp = ary[j];
+        ary[j] = ary[j+1];
+        ary[j+1] = tmp;
+      }
+    }
+  }
+}
+
+int insertion_sort(int *ary, int anum, int asc) {
+  int i, j, tmp;
+  for(i=1; i<anum; ++i) {
+    for(j=i; j>=1; --j) {
+      if(( asc && ary[j-1] > ary[j]) ||
+         (!asc && ary[j-1] < ary[j])){
+        tmp = ary[j-1];
+        ary[j-1] = ary[j];
+        ary[j] = tmp;
+      }
+      else
+        break;
+    }
+  }
+}
+
+
+
+
+int is_sorted(int *ary, int anum, int asc) {
   int i;
-  for(i=0; i<anum-1; ++i) 
-  {
-    if(( asc && ary[i] > ary[i+1]) || (!asc && ary[i] < ary[i+1]))  
-       {
+  for(i=0; i<anum-1; ++i) {
+    if(( asc && ary[i] > ary[i+1]) ||
+       (!asc && ary[i] < ary[i+1]))  {
       return 0;
     }
   }
